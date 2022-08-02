62eeebcc5f6413d33c7d02e216113bbf794c65b1
nohaaltayeb <nohaaltayeb99@gmail.com>
2022-08-02 19:17:07 +0200

    editing firewall

--- infra-Terraform/firewall.tf
+++ infra-Terraform/firewall.tf
@@ -3,7 +3,7 @@ resource "google_compute_firewall" "allow-ssh" {
   name        = "allow-ssh"
   network     =  google_compute_network.vpc_network.id
   direction     = "INGRESS"
-  source_ranges =  [ "0.0.0.0/0" ]
+  source_ranges =  [ "0.0.0.0/0"  ]
 
   description = "Creates firewall rule for instances"
   depends_on = [
